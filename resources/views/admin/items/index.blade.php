b01f871a6f8432f8d77aa49646a290f6c40847bc
RiccardoMacchi <rick.mac97@outlook.com>
2024-09-23 14:58:58 +0200

    added index table for all items

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -2,10 +2,27 @@
 
 @section('content')
     <div class="container">
-        <ul>
-            @foreach ($items as $item)
-                <li>{{ $item }}</li>
-            @endforeach
-        </ul>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Linguaggi</th>
+                    <th scope="col">Link a GitHub</th>
+                    <th scope="col">Descrizione</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                    <tr>
+                        <th>{{ $item->id }}</th>
+                        <td>{{ $item->title }}</td>
+                        <td>{{ $item->lenguages }}</td>
+                        <td>{{ $item->git_link }}</td>
+                        <td>{{ $item->description }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <aside class="text-bg-dark">
     <ul>
         <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a></li>
-        <li><a href="{{ route('admin.allitems') }}"><i class="fa-solid fa-list"></i> Elenco Post</a></li>
-        <li><a href=""></a><i class="fa-solid fa-newspaper"></i> Nuovo Post</li>
+        <li><a href="{{ route('admin.allitems') }}"><i class="fa-solid fa-list"></i> Elenco Lavori</a></li>
+        <li><a href=""></a><i class="fa-solid fa-newspaper"></i> Aggiungi Lavoro</li>
     </ul>
 </aside>
